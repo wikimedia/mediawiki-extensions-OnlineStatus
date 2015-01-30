573a3258bb3d22e5b94cd2fc84358040f07232e8
Ricordisamoa <ricordisamoa@openmailbox.org>
2015-01-30 19:50:03 +0100

    Add COPYING and 'license-name' matching SPDX conventions
    References:
    http://spdx.org/licenses/
    https://www.mediawiki.org/wiki/Manual:$wgExtensionCredits#license-name
    
    Change-Id: Ice3b1cdac961f1073ed19da59f524d290d062023

--- OnlineStatus.php
+++ OnlineStatus.php
@@ -20,6 +20,7 @@ $wgExtensionCredits['other'][] = array(
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:OnlineStatus',
 	'version'        => '2014-04-01',
 	'descriptionmsg' => 'onlinestatus-desc',
+	'license-name'   => 'GPL-2.0+',
 );
 
 // Configuration
